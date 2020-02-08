962fb8243ce4779b11fa86405e9d687c700a05c8
Ricardo Gonzalez Villegas <rikarudogonzarezu@gmail.com>
2020-02-08 22:29:41 +0900

    completed javascript

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,15 @@
 "use strict";
 let totalDisaply = document.getElementById("total-display");
-let historyDisplay = document.getElementById("history-display");
 let operator = "";
 let intergerString = "0";
 let firstInterger = "";
 let secondInterger = "";
 let total = "";
-let history = "";
 let isCalc = true;
-let allowOperator = true;
-let caseOne = false;
-let caseTwo = false;
 
 function equals() {
   setInterger();
   chooseOperation(operator);
-  allowOperator = true;
 }
 
 function toInt(str) {
@@ -29,55 +23,31 @@ function setInterger() {
     !firstInterger
       ? (firstInterger = intergerString)
       : (secondInterger = intergerString);
-    setHistoryInterger();
     intergerString = "";
   }
 }
 
 function add() {
-  console.log("first " + firstInterger);
-  console.log("second " + secondInterger);
   if (isCalc) {
     total = toInt(firstInterger) + toInt(secondInterger);
     isCalc = false;
-    allowOperator = false;
-    console.log("i got to the orignal");
   } else if (!secondInterger) {
     total += toInt(firstInterger);
-    console.log(" i got to the first part");
-    caseOne = true;
-    setHistoryInterger();
-    setHistoryOperator();
   } else if (isCalc === false) {
-    console.log(" i got to the second part");
     total += toInt(secondInterger);
-    caseTwo = true;
-    setHistoryOperator();
-    setHistoryInterger();
   }
   setDisplay();
   return total;
 }
 
 function subtract() {
-  console.log("first " + firstInterger);
-  console.log("second " + secondInterger);
   if (isCalc) {
     total = toInt(firstInterger) - toInt(secondInterger);
     isCalc = false;
-    console.log("i got to the original");
   } else if (!secondInterger) {
     total -= toInt(firstInterger);
-    console.log(" i got to the first part");
-    caseOne = true;
-    setHistoryInterger();
-    setHistoryOperator();
   } else if (isCalc === false) {
-    console.log(" i got to the second part");
     total -= toInt(secondInterger);
-    caseTwo = true;
-    setHistoryOperator();
-    setHistoryInterger();
   }
   setDisplay();
   return total;
@@ -85,51 +55,77 @@ function subtract() {
 
 function divide() {
   if (secondInterger == 0) {
-    history = "";
-    historyDisplay.innerHTML = history;
     totalDisaply.innerHTML = "ERROR";
     return;
   }
-  console.log("first " + firstInterger);
-  console.log("second " + secondInterger);
   if (isCalc) {
     total = toInt(firstInterger) / toInt(secondInterger);
     isCalc = false;
   } else if (!secondInterger) {
     total /= toInt(firstInterger);
-    console.log(" i got to the first part");
-    caseOne = true;
-    setHistoryInterger();
-    setHistoryOperator();
   } else if (isCalc === false) {
-    console.log(" i got to the second part");
     total /= toInt(secondInterger);
-    caseTwo = true;
-    setHistoryOperator();
-    setHistoryInterger();
   }
   setDisplay();
   return total;
 }
 
 function multiply() {
-  console.log("first " + firstInterger);
-  console.log("second " + secondInterger);
   if (isCalc) {
     total = toInt(firstInterger) * toInt(secondInterger);
     isCalc = false;
   } else if (!secondInterger) {
     total *= toInt(firstInterger);
-    console.log(" i got to the first part");
-    caseOne = true;
-    setHistoryInterger();
-    setHistoryOperator();
   } else if (isCalc === false) {
-    console.log(" i got to the second part");
     total *= toInt(secondInterger);
-    caseTwo = true;
-    setHistoryOperator();
-    setHistoryInterger();
+  }
+  setDisplay();
+  return total;
+}
+
+function getSquared() {
+  setInterger();
+  if (isCalc) {
+    total = toInt(firstInterger) * toInt(firstInterger);
+    isCalc = false;
+  } else if (!isCalc) {
+    total *= total;
+  }
+  setDisplay();
+  return total;
+}
+
+function getSquareroot() {
+  setInterger();
+  if (isCalc) {
+    total = Math.sqrt(toInt(firstInterger));
+    isCalc = false;
+  } else if (!isCalc) {
+    total = Math.sqrt(total);
+  }
+  setDisplay();
+  return total;
+}
+
+function getFraction() {
+  setInterger();
+  if (isCalc) {
+    total = 1 / toInt(firstInterger);
+    isCalc = false;
+  } else if (!isCalc) {
+    total = 1 / total;
+  }
+  setDisplay();
+  return total;
+}
+
+function getPercentage() {
+  setInterger();
+  if (isCalc) {
+    total = toInt(firstInterger) / 100;
+    isCalc = false;
+  } else if (!isCalc) {
+    total /= 100;
   }
   setDisplay();
   return total;
@@ -153,11 +149,14 @@ function createInterger(interger) {
     intergerString += interger;
   }
   totalDisaply.innerHTML = intergerString;
-  console.log(intergerString);
 }
 
 function setDisplay() {
-  totalDisaply.innerHTML = Math.round((total + Number.EPSILON) * 100) / 100;
+  if (isNaN(total)) {
+    totalDisaply.innerHTML = "ERROR";
+  } else {
+    totalDisaply.innerHTML = Math.round((total + Number.EPSILON) * 100) / 100;
+  }
 }
 
 function clearEverything() {
@@ -184,53 +183,12 @@ function backspace() {
 }
 
 function addPositiveOrNegative() {
-  intergerString = intergerString * -1;
-  totalDisaply.innerHTML = intergerString;
-}
-
-function setHistoryInterger() {
-  let storeInt = "";
-  if (caseOne) {
-    if (firstInterger < 0) {
-      storeInt = "(" + firstInterger + " )";
-      history += storeInt;
-      historyDisplay.innerHTML = history;
-    } else {
-      history += firstInterger;
-      historyDisplay.innerHTML = history;
-    }
-    caseOne = false;
-  } else if (caseTwo) {
-    if (secondInterger < 0) {
-      storeInt = "(" + secondInterger + " )";
-      history += storeInt;
-      historyDisplay.innerHTML = history;
-    } else {
-      history += secondInterger;
-      historyDisplay.innerHTML = history;
-    }
-    caseTwo = false;
+  if (total) {
+    total = total * -1;
+    totalDisaply.innerHTML = Math.round((total + Number.EPSILON) * 100) / 100;
   } else {
-    if (intergerString < 0) {
-      storeInt = "(" + intergerString + " )";
-      history += storeInt;
-      historyDisplay.innerHTML = history;
-    } else {
-      history += intergerString;
-      historyDisplay.innerHTML = history;
-    }
-  }
-}
-
-function setHistoryOperator() {
-  console.log(history);
-   if (allowOperator) {
-    history += operator;
-    if(history.includes("  ")){
-      history = history.substring(0, history.length-2);
-    }
-    historyDisplay.innerHTML = history;
-    console.log(history);
+    intergerString = intergerString * -1;
+    totalDisaply.innerHTML = intergerString;
   }
 }
 
@@ -242,8 +200,6 @@ numeralButtons.forEach(button => {
     if (isCalc === false) {
       firstInterger = "";
       secondInterger = "";
-      history = "";
-      historyDisplay.innerHTML = history;
       isCalc = true;
     }
   });
@@ -253,7 +209,6 @@ const nonNumeralButtons = document.querySelectorAll(".operator");
 
 nonNumeralButtons.forEach(button => {
   button.addEventListener("click", event => {
-    console.log(event.target.dataset.value);
     operator = event.target.dataset.value;
     if (isCalc === false) {
       firstInterger = total;
@@ -261,30 +216,88 @@ nonNumeralButtons.forEach(button => {
       isCalc = true;
     }
     setInterger();
-    setHistoryOperator();
-    allowOperator = false;
   });
 });
+
+window.addEventListener("keydown", checkKey);
+
+function checkKey(e) {
+  const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+  if (!key) return;
+  switch (key.dataset.value) {
+    case "=":
+      equals();
+      break;
+
+    case "n":
+      addPositiveOrNegative();
+      break;
+
+    case "ce":
+      clearEverything();
+      break;
+
+    case "c":
+      clearInterger();
+      break;
+
+    case "b":
+      backspace();
+      break;
+
+    case "r":
+      getSquareroot();
+      break;
+
+    case "s":
+      getSquared();
+      break;
+
+    case "p":
+      getPercentage();
+      break;
+
+    case "f":
+      getFraction();
+      break;
+  }
+  if (
+    (key.dataset.value >= 0 && key.dataset.value <= 9) ||
+    key.dataset.value == "."
+  ) {
+    createInterger(key.dataset.value);
+    if (isCalc === false) {
+      firstInterger = "";
+      secondInterger = "";
+      isCalc = true;
+    }
+  }
+  if (key.classList.contains("operator")) {
+    if (isCalc === false) {
+      firstInterger = total;
+      secondInterger = "";
+      isCalc = true;
+    }
+    operator = key.dataset.value;
+    setInterger();
+  }
+}
 
 function chooseOperation(operator) {
   switch (operator) {
     case "+":
-      console.log("add");
       total = add();
       break;
 
     case "-":
-      console.log("subtract");
       total = subtract();
       break;
 
     case "/":
-      console.log("divide");
       total = divide();
       break;
 
     case "*":
-      console.log("multiply");
       total = multiply();
       break;
   }
